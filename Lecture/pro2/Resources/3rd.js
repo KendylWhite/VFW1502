47f2078a355af5ebe7732cda6f023e66006801ff
KendylWhite <Kendyl.White.322@gmail.com>
2015-03-10 13:35:27 -0400

    Final

---
+++ Lecture/pro2/Resources/3rd.js
@@ -0,0 +1,84 @@
+var winMList = Ti.UI.createWindow({
+	backgroundColor: "#f1e904"
+});
+var list = Ti.UI.createListView({
+	backgroundColor: "#f1e904",
+	top: 25,
+});
+
+var listSections = [];
+var  actionList = Ti.UI.createListSection({
+	headerTitle: "Action",
+});
+var comedyList = Ti.UI.createListSection({
+	headerTitle: "Comedy",
+});
+var fictionList = Ti.UI.createListSection({
+	headerTitle: "Fiction",
+});
+
+
+//Create Items
+actionListData= [
+	{properties: {title: "Fast & Furious 6", image: "fast6.jpg"}},
+	{properties: {title: "G.I. Joe: Retaliation", image: "joe.jpg"}},
+	{properties: {title: "The Equalizer", image: "equal.jpg"}},
+	{properties: {title: "Dracula Untold", image: "dracula.jpg"}},
+	{properties: {title: "American Sniper", image: "sniper.jpg"}},
+];
+actionList.setItems(actionListData);
+
+var getDetail = function(item){
+var winFun = Ti.UI.createWindow({
+	backgroundColor: "#f1e904",
+});
+var item = Ti.UI.createWindow({
+	text: "Movies",
+	backgroundImage: listSections[i].image,
+});
+winFun.add(item);
+winM.openWindow(winFun);
+};
+
+
+list.addEventListener('itemclick', function(event){
+	console.log(event.section.getItemAt(event.itemIndex));
+	getDetail(event.section.getItemAt(event.itemIndex));
+});
+
+
+comedyListData= [
+	{properties: {title: "22 Jump Street", image: "22.jpg"}},
+	{properties: {title: "Let's Be Cops", image: "cops.jpg"}},
+	{properties: {title: "Ted", image: "ted.jpg"}},
+	{properties: {title: "Dumb & Dumber", image: "dumb.jpg"}},
+	{properties: {title: "Anchorman", image: "anchor.jpg"}},
+];
+comedyList.setItems(comedyListData);
+
+fictionListData= [
+	{properties: {title: "Toy Story", image: "toy.jpg"}},
+	{properties: {title: "The Avengers", image: "avengers.jpg"}},
+	{properties: {title: "Planet of the Apes", image: "apes.jpg"}},
+	{properties: {title: "Transformers", image: "transform.jpg"}},
+	{properties: {title: "Gravity", image: "gravity.jpg"}},
+];
+fictionList.setItems(fictionListData);
+
+//Set Data
+listSections.push(actionList, comedyList, fictionList);
+list.sections = listSections;
+
+var buttonML = Titanium.UI.createButton({
+    title: 'Movie List',
+    top: 215,
+});
+
+buttonML.addEventListener('click', function(){
+    winM.openWindow(winMList, {animated:true});
+});
+
+winMovies.add(buttonML);
+
+winMList.add(list);
+winM.open();
---
+++ Lecture/pro2/Resources/app.js
@@ -0,0 +1,91 @@
+//Window
+var win = Ti.UI.createWindow({
+	backgroundColor: "#f1e904"
+});
+
+//Windows
+var winMovies = Titanium.UI.createWindow({
+    backgroundColor: '#f1e904',
+    title: 'Movies'
+});
+
+var winM = Titanium.UI.iOS.createNavigationWindow({
+   window: winMovies
+});
+
+var winAction = Titanium.UI.createWindow({
+    backgroundColor: '#A13E36',
+    title: 'Action'
+});
+
+var winComedy = Titanium.UI.createWindow({
+    backgroundColor: '#AC7539',
+    title: 'Comedy'
+});
+
+var winFiction = Titanium.UI.createWindow({
+    backgroundColor: '#3EBBB3',
+    title: 'Fiction'
+});
+
+//Buttons
+var buttonA = Titanium.UI.createButton({
+    title: 'Action Table',
+    top: 250,
+});
+var buttonC = Titanium.UI.createButton({
+    title: 'Comedy Table'
+});
+var buttonF = Titanium.UI.createButton({
+    title: 'Fiction Table',
+    bottom: 250,
+});
+
+buttonA.addEventListener('click', function(){
+    winM.openWindow(winAction, {animated:true});
+});
+
+buttonC.addEventListener('click', function(){
+    winM.openWindow(winComedy, {animated:true});
+});
+
+buttonF.addEventListener('click', function(){
+    winM.openWindow(winFiction, {animated:true});
+});
+
+winMovies.add(buttonA);
+var buttonAc = Titanium.UI.createButton({
+    title: 'Close Action'
+});
+
+winMovies.add(buttonC);
+var buttonCo = Titanium.UI.createButton({
+    title: 'Close Comedy'
+});
+
+winMovies.add(buttonF);
+var buttonFi = Titanium.UI.createButton({
+    title: 'Close Fiction'
+});
+
+buttonAc.addEventListener('click', function(){
+    winM.closeWindow(buttonAction, {animated:false});
+});
+
+buttonCo.addEventListener('click', function(){
+    winM.closeWindow(winComedy, {animated:false});
+});
+
+buttonFi.addEventListener('click', function(){
+    winM.closeWindow(winFiction, {animated:false});
+});
+
+winAction.add(buttonAc);
+winComedy.add(buttonCo);
+winFiction.add(buttonFi);
+winM.open();
+
+//Place on screen
+require("2nd");
+require("3rd");
+winM.open();
